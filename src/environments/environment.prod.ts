c65c41b888ed0cf741b255624897c501de9c91b5
mariobeltran99 <mariobeltran1999@gmail.com>
2020-09-30 15:21:25 -0600

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD40SyzPNozsswpzvmgZqhDiDU0-bCx5yM",
+    authDomain: "taller-mecanico-587b7.firebaseapp.com",
+    databaseURL: "https://taller-mecanico-587b7.firebaseio.com",
+    projectId: "taller-mecanico-587b7",
+    storageBucket: "taller-mecanico-587b7.appspot.com",
+    messagingSenderId: "548959107755",
+    appId: "1:548959107755:web:b1be03d85827a924b415ee",
+    measurementId: "G-D6019R4XP9"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD40SyzPNozsswpzvmgZqhDiDU0-bCx5yM",
+    authDomain: "taller-mecanico-587b7.firebaseapp.com",
+    databaseURL: "https://taller-mecanico-587b7.firebaseio.com",
+    projectId: "taller-mecanico-587b7",
+    storageBucket: "taller-mecanico-587b7.appspot.com",
+    messagingSenderId: "548959107755",
+    appId: "1:548959107755:web:b1be03d85827a924b415ee",
+    measurementId: "G-D6019R4XP9"
+  }
 };
 
 /*
