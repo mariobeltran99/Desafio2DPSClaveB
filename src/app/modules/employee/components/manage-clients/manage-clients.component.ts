b99a56d451dad6690f066a1194c3196b5db7d625
mariobeltran99 <mariobeltran1999@gmail.com>
2020-10-01 21:36:24 -0600

    design clients

--- src/app/modules/employee/components/manage-clients/manage-clients.component.ts
+++ src/app/modules/employee/components/manage-clients/manage-clients.component.ts
@@ -1,15 +1,92 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-manage-clients',
   templateUrl: './manage-clients.component.html',
-  styleUrls: ['./manage-clients.component.css']
+  styleUrls: ['./manage-clients.component.css'],
 })
 export class ManageClientsComponent implements OnInit {
+  clientForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.clientForm = this.fb.group({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/
+        ),
+        Validators.minLength(2),
+        Validators.maxLength(60),
+      ]),
+      dui: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]'),
+      ]),
+      marca: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(60),
+      ]),
+      model: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(60),
+      ]),
+    });
   }
+  isFieldValid1(field: string) {
+    const client = this.clientForm.get(field);
+    return (client.touched || client.dirty) && !client.valid;
+  }
+  getErrorMessage1(field: string): string {
+    let message;
+    const forms = this.clientForm.get(field);
+    if (forms.hasError('required')) {
+      message = 'El campo es requerido.';
+    }
+    switch (field) {
+      case 'name':
+        if (forms.hasError('pattern')) {
+          message = 'Solo se aceptan letras';
+        } else if (forms.hasError('minlength')) {
+          message = 'Debe contener como mínimo 2 caracteres';
+        } else if (forms.hasError('maxlength')) {
+          message = 'Debe contener como máximo 60 caracteres';
+        }
+        break;
+      case 'dui':
+        if (forms.hasError('pattern')) {
+          message = 'Debe tener el formato 00000000-0';
+        }
+        break;
+      case 'marca' || 'model':
+        if (forms.hasError('minlength')) {
+          message = 'Debe contener como mínimo 3 caracteres';
+        } else if (forms.hasError('maxlength')) {
+          message = 'Debe contener como máximo 60 caracteres';
+        }
+      break;
+    }
+    return message;
+  }
+  clients = ['hola'];
+  onSave() {
+    if(this.clientForm.valid){
 
+    }
+  }
+  resetClientForm() {
+    const client = this.clientForm;
+    client.reset();
+    Object.keys(client.controls).forEach((key) => {
+      client.controls[key].setErrors(null);
+    });
+  }
 }
--- src/app/modules/employee/components/view-central/view-central.component.ts
+++ src/app/modules/employee/components/view-central/view-central.component.ts
@@ -2,20 +2,54 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalSignOutComponent } from 'src/app/modules/core/components/modal-sign-out/modal-sign-out.component';
 
 @Component({
   selector: 'app-view-central',
   templateUrl: './view-central.component.html',
-  styleUrls: ['./view-central.component.css']
+  styleUrls: ['./view-central.component.css'],
 })
 export class ViewCentralComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
+  public user$: Observable<any> = this.authServices.afAuth.user;
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private route: ActivatedRoute,
+    private authServices: AuthService,
+    private toastr: ToastrService,
+    private dialog: MatDialog
+  ) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  onSignOut(){
+    const dialogRef = this.dialog.open(ModalSignOutComponent);
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        this.logout();
+      }
+    });
+  }
+  logout() {
+    try {
+      this.authServices.logout();
+      this.router.navigate(['/']);
+    } catch (ex) {
+      this.toastr.warning('Ocurrió un problema al cerrar sesión','Advertencia');
+    }
+  }
+  showManageClient(){
+    this.router.navigate(['manage-clients'], { relativeTo: this.route });
+  }
+  showManageRepair(){
+    this.router.navigate(['manage-repairs'], { relativeTo: this.route });
+  }
 }
